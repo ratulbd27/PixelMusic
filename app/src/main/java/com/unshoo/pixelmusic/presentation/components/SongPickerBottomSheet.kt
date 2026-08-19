2f88264f4d4a1d53c0db57f30867da4438d477d5
ratulbd27 <2.61561746e+08+ratulbd27@users.noreply.github.com>
2026-08-19 18:30:00 +0000

    Opt in to experimental Material 3 APIs

--- app/src/main/java/com/unshoo/pixelmusic/presentation/components/SongPickerBottomSheet.kt
+++ app/src/main/java/com/unshoo/pixelmusic/presentation/components/SongPickerBottomSheet.kt
@@ -1,3 +1,5 @@
+@file:OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
+
 package com.unshoo.pixelmusic.presentation.components
 
 import androidx.annotation.OptIn
--- app/src/main/java/com/unshoo/pixelmusic/presentation/components/subcomps/FetchLyricsDialog.kt
+++ app/src/main/java/com/unshoo/pixelmusic/presentation/components/subcomps/FetchLyricsDialog.kt
@@ -1,3 +1,5 @@
+@file:OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
+
 package com.unshoo.pixelmusic.presentation.components.subcomps
 
 import androidx.compose.foundation.background
